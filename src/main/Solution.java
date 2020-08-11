de28808f82e43588f756ec9580184b8cf042fa3e
yuno99825 <49044003+yuno99825@users.noreply.github.com>
2020-08-11 15:24:54 +1200

    yoh717-ttha724/valid solution implementation (#3)
    * Implemented and initialised data structure to store earliest possible schedule for tasks per processor.
    
    * earliestSceduleTimes update for children/processor
    
    The children of the candidate get their earliest schedule time updated to the finish time of the candidate.
    
    For all nodes, the minimum schedule time on the processor in which the candidate is placed is changed to the maximum of their current earliest schedule time and the finish time of the candidate.
    
    Co-Authored-By: yuno99825 <49044003+yuno99825@users.noreply.github.com>
    
    * Check if child has no more dependencies
    
    Co-Authored-By: yuno99825 <49044003+yuno99825@users.noreply.github.com>
    
    * Fixed earliest scheduling time to compare to current value
    
    Co-authored-by: Tushar Thakur <thakur.tushar0205@gmail.com>

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,6 +1,8 @@
 package main;
 
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class Solution {
 
@@ -16,16 +18,72 @@ public class Solution {
      * @return List of scheduled tasks.
      */
     public Task[] run(List<Integer>[] inList, List<Integer>[] outList, int[][] commCosts, int[] durations, int numProcessors) {
-        // TODO
+        int n = inList.length;
+
         // create output array
-        // create list of tasks for each processor
+        Task[] output = new Task[n];
+
+        // i,j indicates earliest time to schedule task i on processor j
+        int[][] earliestScheduleTimes = new int[n][numProcessors];
+
         // initialise array with in degree of vertices
+        int[] inDegrees = new int[n];
 
         // initialise list of vertices with in degree = 0
+        Queue<Integer> scheduleCandidates = new PriorityQueue<>();
 
-        // find a node with in degree 0
+        for (int i = 0; i < n; i++) {
+            inDegrees[i] = inList[i].size();
+            if (inDegrees[i] == 0) {
+                scheduleCandidates.add(i);
+            }
+        }
 
-        // schedule it
-        return null;
+        while (!scheduleCandidates.isEmpty()) {
+            // find a node with in degree 0
+            int candidate = scheduleCandidates.poll();
+
+            // Choose processor to schedule task on
+            int minStartTime = earliestScheduleTimes[candidate][0];
+            int minProcessor = 0;
+            for (int i = 1; i < numProcessors; i++) {
+                int currStartTime = earliestScheduleTimes[candidate][i];
+                if (currStartTime < minStartTime) {
+                    minStartTime = currStartTime;
+                    minProcessor = i;
+                }
+            }
+
+            // Schedule task
+            int finishTime = minStartTime + durations[candidate];
+            output[candidate] = new Task(candidate, minStartTime, finishTime, minProcessor);
+
+            // Update earliest schedule times for children
+            for (int child: outList[candidate]) {
+                for (int i = 0; i < numProcessors; i++) {
+                    if (i == minProcessor) {
+                        // for the processor the candidate was applied to,
+                        // the earliest schedule time could be right after the candidate finishes
+                        earliestScheduleTimes[child][minProcessor] = Math.max(finishTime,
+                                earliestScheduleTimes[child][minProcessor]);
+                    } else {
+                        earliestScheduleTimes[child][i] = Math.max(finishTime + commCosts[candidate][child],
+                                earliestScheduleTimes[child][i]);
+                    }
+                }
+
+                // Decrement in-degree count of child and see if it can be a candidate
+                inDegrees[child]--;
+                if (inDegrees[child] == 0) {
+                    scheduleCandidates.add(child);
+                }
+            }
+            // Update earliest schedule times for the processor which the task was scheduled on (minProcessor)
+            for (int i = 0; i < n; i++) {
+                earliestScheduleTimes[i][minProcessor] = Math.max(finishTime, earliestScheduleTimes[i][minProcessor]);
+            }
+        }
+
+        return output;
     }
 }
