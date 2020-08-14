a3b0e6b60e72900b0462a61359b208214d213ea0
Milk-Yan <eyan868@aucklanduni.ac.nz>
2020-08-14 16:48:34 +1200

    Added code for command line args

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,10 +1,40 @@
 import java.util.List;
+import org.apache.commons.cli.*;
 
 public class Driver {
 
     public static void main(String[] args) {
         String fileName = args[0];
         int numProcessors = Integer.parseInt(args[1]);
+
+        Options options = new Options();
+        Option p = new Option("p", true, "numCores");
+        p.setRequired(false);
+        Option v = new Option("v", false, "visualization");
+        v.setRequired(false);
+        Option o = new Option("o", true, "output");
+        o.setRequired(false);
+
+        options.addOption(p);
+        options.addOption(v);
+        options.addOption(o);
+        CommandLine cmd;
+        CommandLineParser parser = new DefaultParser();
+
+        try {
+            cmd = parser.parse(options, args);
+            int parallel = Integer.parseInt(cmd.getOptionValue('p'));
+            String outputFilePath = cmd.getOptionValue('o', "INPUT-output");
+            boolean visual = cmd.hasOption('v');
+            ///
+            System.out.println(parallel);
+            System.out.println(outputFilePath);
+            System.out.println(visual);
+            ///
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
 
         IOParser io = new IOParser(fileName);
         io.read();
