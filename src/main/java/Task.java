fc4571a1bce9c90018cafdc0340163636b6abdba
Tushar Thakur <thakur.tushar0205@gmail.com>
2020-08-14 21:04:44 +1200

    Comments on Task class (#22)
    * Added comments to explain what the main method does in Driver class
    
    * Added comments to Task class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class to keep track of all information related to a scheduled task
+ */
 public class Task {
     int id;
     int startTime;
@@ -8,6 +11,16 @@ public class Task {
     int processor;
     List<Task> parents = new ArrayList<>();
 
+    /**
+     * When a task is confirmed to be scheduled on a specific processor at a specific time, a Task object relating to
+     * that task is instantiated. This is used in outputting the start time and processor number of each task to the
+     * dot file.
+     *
+     * @param id Unique Identification of the task
+     * @param startTime The start time of the task on a processor
+     * @param finishTime The finish time of the task on a processor
+     * @param processor The processor the task is scheduled on (0 to P-1 where P is the number of processors)
+     */
     public Task(int id, int startTime, int finishTime, int processor) {
         this.id = id;
         this.startTime = startTime;
@@ -25,6 +38,7 @@ public class Task {
 
     public int getId() { return id; }
 
+    // Testing purposes
     public void addParent(Task parent) {
         parents.add(parent);
     }
