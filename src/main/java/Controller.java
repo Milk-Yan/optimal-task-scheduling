a48a70551175d3c61d906987751d0490fe5f3dd2
Tushar Thakur <thakur.tushar0205@gmail.com>
2020-08-25 20:45:19 +1200

    Added timer, ability to modify state and best time. Fixed bugs with task storing
    Co-Authored-By: yuno99825 <49044003+yuno99825@users.noreply.github.com>

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,64 +1,90 @@
+import javafx.animation.AnimationTimer;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.chart.*;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.transform.Rotate;
 
+import javax.xml.soap.Text;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Controller {
     @FXML
     private Pane taskChartContainer;
+    @FXML
+    private Label stateCountLabel;
+    @FXML
+    private Label bestFinishTimeLabel;
+    @FXML
+    private Label timerLabel;
+
     private StackedBarChart stackedBarChart;
     private CategoryAxis xAxis;
 
-    private int[] processorFinishTimes;
-    private XYChart.Series lastTask;
-    private XYChart.Series lastIdle;
-    int count = 0;
+    private Stack<Integer> lastProcessor;
+    private Stack<Integer>[] processorFinishTimes;
 
     public void setUpArgs(int numProcessors) {
-        processorFinishTimes = new int[numProcessors];
-        List<String> processorList = new ArrayList<>();
+        processorFinishTimes = new Stack[numProcessors];
         for (int i = 0; i < numProcessors; i++) {
-            processorList.add("Processor " + (i + 1));
+            processorFinishTimes[i] = new Stack<>();
+            processorFinishTimes[i].push(0);
         }
-        xAxis.setCategories(FXCollections.<String>observableArrayList(processorList));
+        lastProcessor = new Stack<>();
+
+        List<String> xAxisProcessors = new ArrayList<>();
+        for (int i = 0; i < numProcessors; i++) {
+            xAxisProcessors.add("Processor " + (i + 1));
+        }
+        xAxis.setCategories(FXCollections.<String>observableArrayList(xAxisProcessors));
     }
 
     public void addTask(int processor, int duration, int startTime) {
-        int idleTime = startTime - processorFinishTimes[processor];
-        processorFinishTimes[processor] += idleTime + duration;
+        lastProcessor.push(processor);
 
-        lastIdle= new XYChart.Series();
-        lastIdle.getData().add(new XYChart.Data("Processor " + (processor + 1), idleTime));
+        int idleTime = startTime - processorFinishTimes[processor].peek();
+        processorFinishTimes[processor].push(startTime + duration);
 
-        lastTask = new XYChart.Series();
-        lastTask.getData().add(new XYChart.Data("Processor " + (processor + 1), duration));
+        XYChart.Series idle = new XYChart.Series();
+        idle.getData().add(new XYChart.Data("Processor " + (processor + 1), idleTime));
 
-        stackedBarChart.getData().add(lastIdle);
-        stackedBarChart.getData().add(lastTask);
+        XYChart.Series task = new XYChart.Series();
+        task.getData().add(new XYChart.Data("Processor " + (processor + 1), duration));
 
-        Node idleNode = stackedBarChart.lookup(".default-color" + count + ".chart-bar");
-        idleNode.setStyle("-fx-bar-fill: rgba(255, 255, 255, 0);");
-        count++;
-        Node taskNode = stackedBarChart.lookup(".default-color" + count + ".chart-bar");
-        taskNode.setStyle("-fx-bar-fill:  #e9c4bc");
-        count++;
+        stackedBarChart.getData().add(idle);
+        stackedBarChart.getData().add(task);
+
+//        Node idleNode = stackedBarChart.lookup(".default-color" + count + ".chart-bar");
+//        System.out.println(count);
+//        idleNode.setStyle("-fx-bar-fill: rgba(255, 255, 255, 0);");
+//        count++;
+//        Node taskNode = stackedBarChart.lookup(".default-color" + count + ".chart-bar");
+//        taskNode.setStyle("-fx-bar-fill:  #e9c4bc");
+//        count++;
     }
 
     public void removeLast() {
-        count -= 2;
-        stackedBarChart.getData().remove(lastTask);
-        stackedBarChart.getData().remove(lastIdle);
+        stackedBarChart.getData().remove(stackedBarChart.getData().size()-1);
+        stackedBarChart.getData().remove(stackedBarChart.getData().size()-1);
+        processorFinishTimes[lastProcessor.pop()].pop();
+    }
+
+    public void incrementState() {
+        stateCountLabel.setText(String.valueOf(Integer.parseInt(stateCountLabel.getText()) + 1));
+    }
+
+    public void setBestFinishTime(int bestFinishTime) {
+        bestFinishTimeLabel.setText(String.valueOf(bestFinishTime));
     }
 
     @FXML
     private void initialize() {
         xAxis = new CategoryAxis();
-        xAxis.setLabel("Processors");
 
         NumberAxis yAxis = new NumberAxis();
 
@@ -66,5 +92,20 @@ public class Controller {
         stackedBarChart.setLegendVisible(false);
 
         taskChartContainer.getChildren().add(stackedBarChart);
+
+        long startTime = System.currentTimeMillis();
+        new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                long elapsedMillis = System.currentTimeMillis() - startTime ;
+                int seconds = (int) (elapsedMillis / 1000);
+                int minutes = seconds / 60;
+
+                String secondsString = seconds < 10 ? "0" + seconds : seconds + "";
+                String minutesString = minutes < 10 ? "0" + minutes : minutes + "";
+
+                timerLabel.setText(minutesString + ":" + secondsString);
+            }
+        }.start();
     }
 }
--- src/main/java/Visualiser.java
+++ src/main/java/Visualiser.java
@@ -1,17 +1,13 @@
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.chart.CategoryAxis;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.StackedBarChart;
-import javafx.scene.chart.XYChart;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 public class Visualiser extends Application {
+
 
     public void run(String[] args) {
         launch(args);
@@ -19,7 +15,6 @@ public class Visualiser extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        System.out.println("here");
         FXMLLoader loader  = new FXMLLoader(getClass().getResource("visualisation-view.fxml"));
         Parent root = loader.load();
 
@@ -31,9 +26,20 @@ public class Visualiser extends Application {
         controller.addTask(2,100,100);
         controller.addTask(0,100,150);
 
+        controller.incrementState();
+        controller.incrementState();
+
+        controller.setBestFinishTime(200);
+
         primaryStage.setTitle("Task Scheduler Visualisation");
-        primaryStage.setScene(new Scene(root, 780, 475));
+        primaryStage.setScene(new Scene(root, 780, 525));
         primaryStage.setResizable(false);
         primaryStage.show();
+
+        controller.addTask(0,100,260);
+        controller.addTask(1,100,100);
+        controller.removeLast();
+        controller.addTask(1,100,1000);
+        controller.addTask(3,100,100);
     }
 }
