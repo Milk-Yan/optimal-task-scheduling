9d72281ca5ede4b80dc725e591ae88b3f437f9f6
Milk <50122790+Milk-Yan@users.noreply.github.com>
2020-08-30 22:28:59 +1200

    Added test cases (#112)
    * Added utility code for testing.
    
    * Updated gitIgnore to include test graphs and added test graph.
    
    * Changed format of Tester.
    
    * Commented and Refactored Testing classes.
    
    * Added some edge cases to test.
    
    * Added Test for Maximal Edges
    
    * Added new test cases to Tester
    
    * Remove test that is not ready yet
    
    * Added dense graph test
    
    * Removed comments

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -13,10 +13,36 @@ public class Tester {
             System.getProperty("file.separator") + "test" + System.getProperty("file.separator") + "graphs" +
             System.getProperty("file.separator");
 
-    // --------------------------------------BLACK BOX---------------------------------------------
-
-
     // ================================Tests on ONE processor======================================
+
+    /**
+     * Test an empty graph on one processor. It should give an empty output.
+     */
+    @Test
+    public void testOneProcessorEmptyGraph() {
+        SolutionValidator validator = new SolutionValidator();
+
+        String inputFileName = graphDir + "EmptyGraph.dot";
+        String outputFileName = inputFileName.replace(".dot", "-output.dot");
+        assertTrue(testValidity(validator, inputFileName, 1, outputFileName));
+        assertEquals(0, validator.getBestTime());
+        cleanUp(outputFileName);
+    }
+
+    /**
+     * Test a graph with only one node on one processor. It should give us a graph
+     * with the finish time as the weight of the one node.
+     */
+    @Test
+    public void testOneProcessorOneNode() {
+        SolutionValidator validator = new SolutionValidator();
+
+        String inputFileName = graphDir + "1Node.dot";
+        String outputFileName = inputFileName.replace(".dot", "-output.dot");
+        assertTrue(testValidity(validator, inputFileName, 1, outputFileName));
+        assertEquals(21, validator.getBestTime());
+        cleanUp(outputFileName);
+    }
 
     /**
      * Test a simple graph without edges on one processor
@@ -32,8 +58,141 @@ public class Tester {
         cleanUp(outputFileName);
     }
 
-    // --------------------------------------WHITE BOX----------------------------------------------
+    /**
+     * Test a simple graph on one processor with sequential edges A -> B -> C -> D -> E
+     */
+    @Test
+    public void testOneProcessorSequentialEdges() {
+        SolutionValidator validator = new SolutionValidator();
 
+        String inputFileName = graphDir + "5NodesSequentialEdges.dot";
+        String outputFileName = inputFileName.replace(".dot", "-output.dot");
+        assertTrue(testValidity(validator, inputFileName, 1, outputFileName));
+        assertEquals(154, validator.getBestTime());
+        cleanUp(outputFileName);
+    }
+
+    /**
+     * Test a simple graph on one processor with 7 edges.
+     */
+    @Test
+    public void testOneProcessorSevenEdges() {
+        SolutionValidator validator = new SolutionValidator();
+
+        String inputFileName = graphDir + "5Nodes7Edges.dot";
+        String outputFileName = inputFileName.replace(".dot", "-output.dot");
+        assertTrue(testValidity(validator, inputFileName, 1, outputFileName));
+        assertEquals(154, validator.getBestTime());
+        cleanUp(outputFileName);
+    }
+
+    // ===============================Tests on MULTIPLE processors==================================
+
+    /**
+     * Test an empty graph on multiple processors. It should give an empty output.
+     */
+    @Test
+    public void testMultipleProcessorsEmptyGraph() {
+        SolutionValidator validator = new SolutionValidator();
+
+        String inputFileName = graphDir + "EmptyGraph.dot";
+        String outputFileName = inputFileName.replace(".dot", "-output.dot");
+        assertTrue(testValidity(validator, inputFileName, 4, outputFileName));
+        assertEquals(0, validator.getBestTime());
+        cleanUp(outputFileName);
+    }
+
+    /**
+     * Test a graph with only one node on multiple processors. It should give us a graph
+     * with the finish time as the weight of the one node.
+     */
+    @Test
+    public void testMultipleProcessorsOneNode() {
+        SolutionValidator validator = new SolutionValidator();
+
+        String inputFileName = graphDir + "1Node.dot";
+        String outputFileName = inputFileName.replace(".dot", "-output.dot");
+        assertTrue(testValidity(validator, inputFileName, 2, outputFileName));
+        assertEquals(21, validator.getBestTime());
+        cleanUp(outputFileName);
+    }
+
+    /**
+     * Test a simple graph on multiple processors with sequential edges.
+     * They should end up being scheduled on one processor due to
+     * dependencies.
+     */
+    @Test
+    public void testMultipleProcessorSequentialEdges() {
+        SolutionValidator validator = new SolutionValidator();
+
+        String inputFileName = graphDir + "5NodesSequentialEdges.dot";
+        String outputFileName = inputFileName.replace(".dot", "-output.dot");
+        assertTrue(testValidity(validator, inputFileName, 3, outputFileName));
+        assertEquals(154, validator.getBestTime());
+        cleanUp(outputFileName);
+    }
+
+    /**
+     * Test a simple graph on multiple processors with no edges. The tasks
+     * should be evenly scheduled on the processors.
+     */
+    @Test
+    public void testMultipleProcessorZeroEdges() {
+        SolutionValidator validator = new SolutionValidator();
+
+        String inputFileName = graphDir + "5Nodes0Edges.dot";
+        String outputFileName = inputFileName.replace(".dot", "-output.dot");
+        assertTrue(testValidity(validator, inputFileName, 4, outputFileName));
+        assertEquals(46, validator.getBestTime());
+        cleanUp(outputFileName);
+    }
+
+    /**
+     * Test a graph on multiple processors with maximal edges, i.e. if any further
+     * edge is added, the graph would contain a cycle and no longer be valid.
+     * The graph should then be scheduled the same as a sequential graph.
+     */
+    @Test
+    public void testMultipleProcessorsMaximalEdges() {
+        SolutionValidator validator = new SolutionValidator();
+
+        String inputFileName = graphDir + "5NodesMaximalEdges.dot";
+        String outputFileName = inputFileName.replace(".dot", "-output.dot");
+        assertTrue(testValidity(validator, inputFileName, 4, outputFileName));
+        assertEquals(154, validator.getBestTime());
+        cleanUp(outputFileName);
+    }
+
+    /**
+     * Test a sparse graph with fifteen nodes in it. Sparse graph means a graph
+     * with not many edges.
+     */
+    @Test
+    public void testSparseGraphFifteenNodes() {
+        SolutionValidator validator = new SolutionValidator();
+
+        String inputFileName = graphDir + "15Nodes10Edges.dot";
+        String outputFileName = inputFileName.replace(".dot", "-output.dot");
+        assertTrue(testValidity(validator, inputFileName, 3, outputFileName));
+        assertEquals(197, validator.getBestTime());
+        cleanUp(outputFileName);
+    }
+
+    /**
+     * Test a dense graph with fifteen nodes in it. Dense graph means a graph
+     * with many edges.
+     */
+    @Test
+    public void testDenseGraphFifteenNodes() {
+        SolutionValidator validator = new SolutionValidator();
+
+        String inputFileName = graphDir + "15Nodes80Edges.dot";
+        String outputFileName = inputFileName.replace(".dot", "-output.dot");
+        assertTrue(testValidity(validator, inputFileName, 4, outputFileName));
+        assertEquals(516, validator.getBestTime());
+        cleanUp(outputFileName);
+    }
 
     // ----------------------------------------UTILITY-----------------------------------------------
 
